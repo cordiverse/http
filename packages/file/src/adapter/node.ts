578a099a49bf7d4157f7e951cbbf1fbaa7eef404
Shigma <shigma10826@gmail.com>
2024-04-04 00:39:19 +0800

    Update packages/file/src/adapter/node.ts

--- packages/file/src/adapter/node.ts
+++ packages/file/src/adapter/node.ts
@@ -10,6 +10,7 @@ export async function loadFile(url: string): Promise<FileResponse | undefined> {
   if (url.startsWith('file://')) {
     const data = await readFile(fileURLToPath(url))
     const result = await fromBuffer(data)
+    // https://stackoverflow.com/questions/8609289/convert-a-binary-nodejs-buffer-to-javascript-arraybuffer#answer-31394257
     const buffer = data.buffer.slice(data.byteOffset, data.byteOffset + data.byteLength)
     return { mime: result?.mime, filename: basename(url), data: buffer }
   }
